e0d278d9d61ac86644e5716bc941b7676bf7d0a3
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-08-07 21:48:25 -0300

    fileReader & bufferedReader

--- files-with-java/src/application/Program.java
+++ files-with-java/src/application/Program.java
@@ -1,26 +1,40 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		File file = new File("c:\\tmp\\in.txt");
-		Scanner sc = null;
-		
+		String path = "c:\\tmp\\in.txt";
+		BufferedReader br = null;
+		FileReader fr = null;
+				
 		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 		} catch (IOException e) {
 			// TODO: handle exception
 			System.out.println("Error: "+ e.getMessage());
 		} finally {
-			if(sc != null) {
-				sc.close();
-			}				
+			try {
+				if(br != null) {
+					br.close();
+				}
+				if(fr != null){
+					fr.close();
+				}
+			}catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
 		}
 		
 	}
