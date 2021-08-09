5ff133150c37aea10e209686815a4f5cbb73ce4b
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-08-08 22:08:06 -0300

    manipulando pastas com File

---
+++ files-with-java/src/appfolders/Program.java
@@ -0,0 +1,34 @@
+package appfolders;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		//capture all folders in this path
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		//capture all folders in this path
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir(); //create a folder from  "strPath" folder
+		System.out.println("Directory created successfully: " + success);
+		
+		sc.close();
+	}
+}
