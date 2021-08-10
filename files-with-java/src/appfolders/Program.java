36c0b333f241dcff9cd3f29d6af2ae18cf18d752
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-08-09 22:00:45 -0300

    Informações do caminho do arquivo

--- files-with-java/src/appfolders/Program.java
+++ files-with-java/src/appfolders/Program.java
@@ -8,26 +8,14 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
 		
-		//capture all folders in this path
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for(File folder : folders) {
-			System.out.println(folder);
-		}
-		//capture all folders in this path
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for(File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean success = new File(strPath + "\\subdir").mkdir(); //create a folder from  "strPath" folder
-		System.out.println("Directory created successfully: " + success);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 		
 		sc.close();
 	}
