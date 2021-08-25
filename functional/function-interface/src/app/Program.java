d970f8fd4a05573492a3a281f5c5aee6c1698362
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-08-24 23:03:04 -0300

    Implementação da interface

---
+++ functional/function-interface/src/app/Program.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("HD", 80.90));
+
+		//map function
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
+		
+		
+		
+	}
+}
