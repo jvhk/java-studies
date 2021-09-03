c1635fdc97895c8d0b8f3d257d67a4361332b79f
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-09-02 21:16:44 -0300

    Demo - Tratandando database integrity no database

---
+++ database_jdbc/jdbc6/src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbExcecption;
+import db.DbIntegrityException;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		Statement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+			
+			/* just for test DbException
+			int x = 1;
+			if (x<2) {
+				throw new SQLException("Fake error");  
+			}
+			*/
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("rows1 = " + rows1);
+			System.out.println("rows2 = " + rows2);
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			try {
+				conn.rollback();
+				throw new DbExcecption("Transation rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				throw new DbExcecption("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
