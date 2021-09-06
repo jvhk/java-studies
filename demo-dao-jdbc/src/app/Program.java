d41cdbb99c2dbead9c4f18ab4d4f471193f11b12
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-09-05 22:51:58 -0300

    Implementando findById

--- demo-dao-jdbc/src/app/Program.java
+++ demo-dao-jdbc/src/app/Program.java
@@ -1,21 +1,17 @@
 package app;
 
-import java.util.Date;
-
 import models.dao.DAOFactory;
 import models.dao.SellerDAO;
-import models.entities.Department;
 import models.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com",new Date(), 3000.0, obj);
-		System.out.println(seller);
 		
 		SellerDAO sellerDao = DAOFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 		
 	}
 }
--- demo-dao-jdbc/src/models/dao/DAOFactory.java
+++ demo-dao-jdbc/src/models/dao/DAOFactory.java
@@ -1,10 +1,11 @@
 package models.dao;
 
+import db.DB;
 import models.dao.impl.SellerDaoJDBC;
 
 public class DAOFactory {
 	
 	public static SellerDAO createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
