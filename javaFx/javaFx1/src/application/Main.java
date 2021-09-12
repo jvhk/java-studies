221d17bf8b44a586ee2b0d142e1de291cf48119a
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-09-11 21:05:01 -0300

    Testando uma tela básica com fxml

--- javaFx/javaFx1/src/application/Main.java
+++ javaFx/javaFx1/src/application/Main.java
@@ -1,18 +1,19 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+			Scene scene = new Scene(parent);
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- javaFx/javaFx1/src/module-info.java
+++ javaFx/javaFx1/src/module-info.java
@@ -1,5 +1,6 @@
 module javaFx1 {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
